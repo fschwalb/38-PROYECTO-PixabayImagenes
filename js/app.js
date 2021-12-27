92d9d87d1d6a365dfca4e65a7ca06d1dae4d6acf
Fernando Schwalb <fschwalb@gmail.com>
2021-12-27 20:35:26 -0300

    Buscar & Mostrar Imagenes OK

--- js/app.js
+++ js/app.js
@@ -15,6 +15,8 @@ function validarFormulario( e ) {
         
         return;
     }
+
+    buscarImagenes( terminoBusqueda );
 };
 
 function mostrarAlerta( mensaje ) {
@@ -39,5 +41,56 @@ function mostrarAlerta( mensaje ) {
 
     };
 
+
+};
+
+function buscarImagenes( termino ) {
+
+    const key = '24865845-a4191cf0b41050708f2e45ba5';
+    const url = `https://pixabay.com/api/?key=${ key }&q=${ termino }&per_page=100`;
+
+    fetch(url)
+        .then( respuesta => respuesta.json() )
+        .then( resultado => {
+            mostrarImagenes( resultado.hits );
+        })
+
+};
+
+function mostrarImagenes( imagenes ) {
+
+    while( resultado.firstChild ) {
+        resultado.removeChild( resultado.firstChild );
+    };
+
+    // Iterar sobre el arreglo de imágenes y construir el HTML
+    imagenes.forEach( imagen => {
+
+        const { previewURL, likes, views, largeImageURL } = imagen;
+
+        resultado.innerHTML += `
+        <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+            <div class="bg-white">
+                <img class="w-full" src="${ previewURL }">
+
+                <div class="p-4">
+                    <p class="font-bold"> ${ likes } <span class="font-light"> Me Gusta </span> </p>
+                    <p class="font-bold"> ${ views } <span class="font-light"> Veces Vista </span> </p>
+
+                    <a 
+                        class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                        href="${ largeImageURL }" 
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    >
+                        Ver Imágen
+                    </a>
+                </div>
+            
+            </div>        
+        </div>
+        `;
+
+    });
 
 };
